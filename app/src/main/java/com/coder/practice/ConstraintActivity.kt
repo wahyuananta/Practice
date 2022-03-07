ad27b917baa78445cfd227eb3b3e445c5a532a7e
wahyuananta <wahyuanantajr@gmail.com>
2022-03-07 21:05:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/coder/practice/ConstraintActivity.kt
@@ -0,0 +1,11 @@
+package com.coder.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
---
+++ app/src/main/java/com/coder/practice/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.coder.practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val name = "Wahyu"
+        Log.d("AND-4", "Selamat Pagi $name")
+    }
+}
